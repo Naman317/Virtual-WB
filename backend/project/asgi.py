34b621850e5538da420596f6c82eecde93f84cb4
unknown <namansharma3194@gmail.com>
2026-09-23 12:00:21 +0530

    Fix ASGI initialization order

--- backend/project/asgi.py
+++ backend/project/asgi.py
@@ -2,12 +2,14 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import boards.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+django_asgi_app = get_asgi_application()
+
+import boards.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             boards.routing.websocket_urlpatterns
