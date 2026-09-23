707d23e3abf01e596bf72ae08ead4a97a85713f5
unknown <namansharma3194@gmail.com>
2026-09-23 12:53:12 +0530

    Remove old unrelated repository files

--- chat/routing.py
+++
@@ -1,9 +0,0 @@
-"""
-Chat WebSocket routing
-"""
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<session_code>\w+)/$', consumers.ChatConsumer.as_asgi()),
-]
